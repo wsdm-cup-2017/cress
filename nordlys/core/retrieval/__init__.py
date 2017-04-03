8ee1398cffb0d7c40a7a764812b3934276726980
hasibi <hasibi.44@gmail.com>
2017-04-03 12:09:44 +0200

    elastic and file_utils

---
+++ nordlys/core/retrieval/__init__.py
@@ -0,0 +1,18 @@
+"""This is the (**intro** of the) retrieval package doc. It's in the ``__init__.py`` of the package, as an example for the other packages, and IT SHOULD BE cleaned up without all of this ;).
+
+It could contain similar/the same retrieval best practices text.
+
+It might contain, e.g., items in this way
+
+* This
+
+* That
+
+and also code snippets like usual, using as usual these double colons and the indented code::
+
+    retrieval = Retrieval(config)
+
+And that's it.
+
+(In the following I included all the up-to-now available modules, sectioned by the best practices page criteria.)
+"""
